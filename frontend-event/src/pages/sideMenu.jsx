888058328d4ab6b1301987f4635d1d79dab30705
Fredrich Benedetti <fredrich93@gmail.com>
2024-11-03 21:45:26 +0100

    settings id put down and User if added
    Next task is to but info button next to settings

--- frontend-event/src/pages/sideMenu.jsx
+++ frontend-event/src/pages/sideMenu.jsx
@@ -25,23 +25,29 @@ function SideMenuLayout({ buttons }) {
         <div className={`swipe-menu ${isOpen ? 'open' : ''}`} {...handlers}>
             {isOpen && (
            <div className='sidebar'>
+            <div className='sidebar_userName #CE9F9F font-extrabold text-black text-5xl hover:shadow-xl h-full' onClick={e => e.stopPropagation()}>
+                <button>User</button>
+            </div>
                 <div className="sidebar_buttons #CE9F9F p-4 text-white h-full" onClick={e => e.stopPropagation()}>
                     {buttons.map((button, index) => (
                         <button
                             key={index}
-                            className={`${button.className} w-full text-left mb-2 py-2 px-4 rounded`}
+                            className={`${button.className} w-full mb- py-4 px-10 rounded`}
                             onClick={button.onClick}
-                            style={{ fontSize: '14px' }} // Adjust the font size as needed
+                            style={{ fontSize: '20px' }} /* För att ändra textstorlek*/
                         >
                             {button.label}
                         </button>
                     ))}
                 </div>
              
-             <div className='sidebar_settings #CE9F9F p-4 bg-red-200 shadow-[inset_0_-1px_2px_rgba(0,0,0,0.6)] hover:bg-red-200 hover:shadow-[inset_0_2px_4px_rgba(0,0,0,0.6)] text-white h-full' onClick={e => e.stopPropagation()}>
+             <div className='sidebar_settings #CE9F9F p-1 bg-red-200 shadow-[inset_0_-1px_2px_rgba(0,0,0,0.6)] hover:bg-red-200 hover:shadow-[inset_0_2px_4px_rgba(0,0,0,0.6)] text-white h-full' onClick={e => e.stopPropagation()}>
                     <button className='sidebar_settings_button'> Settings
                     </button>
 
+                </div>
+                <div className='sidebar_info rounded-full #CE9F9F p-1 bg-red-200 shadow-[inset_0_-1px_2px_rgba(0,0,0,0.6)] hover:bg-red-200 hover:shadow-[inset_0_2px_4px_rgba(0,0,0,0.6)] text-white h-full'>
+                    <button>i</button>
                 </div>
          </div>)}
          
