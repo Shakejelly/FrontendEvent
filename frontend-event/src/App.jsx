422b879daef554afd863e36d52b3b0afba76f56f
Fredrich Benedetti <fredrich93@gmail.com>
2024-11-06 15:27:40 +0100

    SideBar
    Blabla

--- frontend-event/src/App.jsx
+++ frontend-event/src/App.jsx
@@ -4,24 +4,20 @@ import SideMenuLayout from './pages/sideMenu';
 import EventPage from './pages/eventPage';
 
 
+
+
 const buttonsConfig = [
   {
-
     label: "Home",
     className: "bg-red-200 shadow-[inset_0_-1px_2px_rgba(0,0,0,0.6)] hover:bg-red-200 hover:shadow-[inset_0_2px_4px_rgba(0,0,0,0.6)] text-white",
-
   },
   {
-
     label: "Calender",
     className: "bg-red-200 shadow-[inset_0_-1px_2px_rgba(0,0,0,0.6)] hover:bg-red-200 hover:shadow-[inset_0_2px_4px_rgba(0,0,0,0.6)] text-white",
-
   },
   {
-
     label: "Saved",
     className: "bg-red-200 shadow-[inset_0_-1px_2px_rgba(0,0,0,0.6)] hover:bg-red-200 hover:shadow-[inset_0_2px_4px_rgba(0,0,0,0.6)] text-white",
-
   },
   {
     label: "Sport",
@@ -31,7 +27,16 @@ const buttonsConfig = [
     label: "Music",
     className: "bg-red-200 shadow-[inset_0_-1px_2px_rgba(0,0,0,0.6)] hover:bg-red-200 hover:shadow-[inset_0_2px_4px_rgba(0,0,0,0.6)] text-white",
   },
-]
+];
+function MainLayout() {
+  return (
+    <div className='main-layout'>
+      <SideMenuLayout buttons={buttonsConfig} />
+      <div className='event-page-container flex-grow-1 h-0 overflow-auto'></div>
+      <EventPage />
+    </div>
+  );
+}
 
 
 function App() {
@@ -40,12 +45,9 @@ function App() {
     <>
       <Router>
         <Routes>
+        <Route path="/" element={<MainLayout />} />
+        <Route path="/MockMenu" element={<div>Mock Menu Content</div>} />
 
-          <Route path="/" element={<EventPage />} />
-
-          <Route path="/" element={<SideMenuLayout buttons={buttonsConfig}/>}>
-            <Route path='/MockMenu' element={<div>Mock Menu Content</div>}></Route >
-          </Route>
 
         </Routes>
       </Router>
