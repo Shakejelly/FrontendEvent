c262844784f24356800c550716ee1f2b25dc6a15
mmxzps <mojtaba.mobasheri@hotmail.com>
2024-11-14 17:27:11 +0100

    Created basic profilepage with limited friends showing.

--- frontend-event/src/pages/userPage.jsx
+++ frontend-event/src/pages/userPage.jsx
@@ -29,28 +29,68 @@ const UserPage = () => {
     return (
         <>
             <div className='userPage min-h-screen bg-DarkPurple flex justify-center item-center'>
+
+                {/* container for welcome,profPic, friendContainer */}
                 <div className="flex flex-col justify-top items-center mt-4 space-y-4">
-                    <h5 className='text-center'>Welcome {user ? user.firstName : "Guest"}</h5>
+
 
                     {user && user.profilePictureUrl ?
                         (<img src={user.profilePictureUrl} alt="ProfilePicture" className="rounded-full w-20 h-20" />)
                         :
                         (<p>No profile picture available</p>)
                     }
-                    <div>
-                        <p>friends:{friends.length}</p>
+                    <h5 className='text-center'>{user ? `${user.firstName} ${user.lastName}` : "Guest"}</h5>
+
+                    {/* friend container */}
+                    <div className="bg-DarkPurple shadow-lg rounded-lg p-6 max-w-md mx-auto mt-8">
+
+                        {/* container for numberofFriends and showAll button */}
+                        <div className='flex flex-row justify-between'>
+
+                            {/* showing amount friends */}
+                            <div>
+                                <p className="text-x font-semibold text-gray-800 mb-4">{friends.length} Friends</p>
+                            </div>
+
+                            {/*Show all button*/}
+                            <div>
+                                <p>Show All</p>
+                            </div>
+                        </div>
+
+                        {/* friend box with friend showing */}
+                        <div className="grid grid-cols-2 gap-4 w-70 h-70">
+
+                            {friends.slice(0, 4).map((friend, index) => (
+
+                                <div key={index} className='bg-DarkPurple shadow-lg rounded-lg p-5 max-w-md mx-auto m-1'>
+                                    <div className='flex justify-center'>
+                                        <img src={friend.profilePictureUrl} alt="FriendProfilePicture" className="w-12 h-12 rounded-md object-cover border-2 border-purple-500" />
+                                    </div>
+                                    <div>
+                                        <p className='break-words flex'>{friend.firstName} {friend.lastName} </p>
+                                    </div>
+                                </div>
+
+                            ))}
+                        </div>
+
+
                     </div>
+
+                    {/* edit profile section */}
                     <div>
-                        <button className="w-auto px-2 py-4 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300">
+                        <button className="bg-red-200 text-black w-full py-4 px-4 rounded shadow-inner hover:bg-red-300 hover:shadow-md">
                             Edit profile
                         </button>
                     </div>
                 </div>
 
-
+                {/* h
                 <div className="userCard w-full max-w-sm flex justify-center max-h-96 mt-5 ml-10"> {/* Set a max-width for the card */}
-                    {/* <UserCard /> */}
-                </div>
+                {/* <UserCard /> */}
+                {/* </div> */}
+
             </div>
         </>
 
