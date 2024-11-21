2a06c09afe7c4a5e432f64e6d2e9f1689566a7e4
mmxzps <mojtaba.mobasheri@hotmail.com>
2024-11-21 19:09:21 +0100

    added aboutus page url

--- frontend-event/src/components/AboutUs.jsx
+++ frontend-event/src/components/AboutUs.jsx
@@ -3,7 +3,7 @@ const AboutUs = () => {
     return (
         <div className='pl-10'>
             <iframe
-                src="https://localhost:7295"
+                src="https://evabout.azurewebsites.net"
                 className="w-full h-screen border-none"
                 title="About Us"
             />
