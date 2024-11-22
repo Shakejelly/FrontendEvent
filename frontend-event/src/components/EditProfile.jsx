3b736c906cea07b078a8f180f535000c0c36c9a7
mmxzps <mojtaba.mobasheri@hotmail.com>
2024-11-22 15:37:54 +0100

    fixed link back button

--- frontend-event/src/components/EditProfile.jsx
+++ frontend-event/src/components/EditProfile.jsx
@@ -70,7 +70,7 @@ const EditProfile = () => {
                             <div className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-Flesh 
                             rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:purpleContrast 
                             dark:hover:bg-blue-700 dark:focus:ring-blue-800 mt-2">
-                                <Link to={`/user/${userId}`} >
+                                <Link to={"/user/"} >
                                     Back
                                 </Link>
                             </div>
