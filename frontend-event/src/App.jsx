068462688865bfaf73b60e0271d32eb1d1d1419c
Skokartong <amanda.olving@gmail.com>
2024-11-27 12:51:24 +0100

    Changed names so they are more uniform

--- frontend-event/src/App.jsx
+++ frontend-event/src/App.jsx
@@ -1,16 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import EventPage from "./pages/EventPage";
-import UserPage from "./pages/UserPage";
+import EventPage from "./pages/MainEventPage";
+import UserPage from "./pages/MainUserPage";
 import FriendsAll from "./components/FriendsAll";
 import EditProfile from "./components/EditProfile";
 import { Link } from "react-router-dom";
-import LandingPage from './pages/LandingPage'
-import AdminPage from "./pages/adminPage";
+import LandingPage from "./pages/MainLandingPage";
+import AdminPage from "./pages/MainAdminPage";
 import AboutUs from "./components/AboutUs";
 import HamburgerLayout from "./components/HamburgerLayout"; // New layout component
-import ForgotPassword from "./pages/ForgotPasswordPage"
+import ForgotPassword from "./pages/ForgotPasswordPage";
 import FriendPage from "./components/FriendPage";
 
 function App() {
